4e15542d8b11a0cf735d6b77895c2bd539bc23d8
leloufadel <leloufadel@gmail.com>
2024-05-07 12:28:36 +0000

    Pop Drilling

--- src/App.js
+++ src/App.js
@@ -50,12 +50,12 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-function NavBar() {
+function NavBar({movies}) {
   return (
     <nav className="nav-bar">
       <Logo />
       <Search />
-      <NumResults />
+      <NumResults movies={movies} />
     </nav>
   );
 }
@@ -68,10 +68,10 @@ function Logo() {
   );
 }
 
-function NumResults() {
+function NumResults({movies}) {
   return (
     <p className="num-results">
-      Found <strong>X</strong> results
+      Found <strong>{movies.length}</strong> results
     </p>
   );
 }
@@ -88,17 +88,17 @@ function Search() {
     />
   );
 }
-function Main() {
+function Main({movies}) {
   return (
     <>
       <main className="main">
-        <ListBox />
+        <ListBox movies={movies}/>
         <WatchedBox />
       </main>
     </>
   );
 }
-function ListBox() {
+function ListBox({movies}) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -109,12 +109,11 @@ function ListBox() {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieLists />}
+      {isOpen1 && <MovieLists movies={movies} />}
     </div>
   );
 }
-function MovieLists() {
-  const [movies, setMovies] = useState(tempMovieData);
+function MovieLists({movies}) {
 
   return (
     <ul className="list">
@@ -232,10 +231,12 @@ function WatchedMovie({ movie }) {
   )
 }
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+
   return (
     <>
-      <NavBar />
-      <Main />
+      <NavBar movies={movies}/>
+      <Main movies={movies}/>
     </>
   );
 }
